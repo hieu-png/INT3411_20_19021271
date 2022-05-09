3a38c769aeb82650281ac6ef6649727eb38d6d78
taiyounari@gmail.com <49590699+taiyounari@users.noreply.github.com>
2022-05-09 22:35:19 +0700

    Updating the correct way to use

--- 1_MFCC_Extraction.py
+++ 1_MFCC_Extraction.py
@@ -5,33 +5,31 @@ import numpy as np
 
 #Reading 100 wav file to extract mel frequency cepstral coefficients
 f  = open('MFCCresults.txt','w')
-print("Starting creating MFCC results")
-for index in range(1,101):
-    print("Currently at file: %d" % (index), end="\r")
-    audio_file = './19021271_NguyenTrungHieu/c%d.wav'%(index)
-    ipd.Audio(audio_file)
-    signal, sr = librosa.load(audio_file)
-    mfccs = librosa.feature.mfcc(y=signal, n_mfcc=13, sr=sr)
+#Just writing the header collumn
+f.writelines('command\tcoefficients\ttime frames\n')
+print('Starting creating MFCC results')
+for index in range(1, 101):
+    print('Currently at file: %d' % (index), end='\r')
+    audio_file_path = './19021271_NguyenTrungHieu/c%d.wav' % (index)
+    command_timestamp_path = './19021271_NguyenTrungHieu/c%d.txt' % (index)
+    command_array = np.genfromtxt(command_timestamp_path, delimiter='\t', dtype='unicode')
+    #print(command_array)
+    for command in command_array:
+        #print(command[0])
+        ipd.Audio(audio_file_path)
+        signal, sr = librosa.load(
+        path=audio_file_path,
+        offset= float(command[0]),
+        duration=float(command[1])-float(command[0])
+        )
+        mfccs = librosa.feature.mfcc(y=signal, n_mfcc=13, sr=sr)
 
-    """ plt.figure(figsize=(25, 10))
-    librosa.display.specshow(mfccs,
-                         x_axis="time",
-                         sr=sr)
-    plt.colorbar(format="%+2.f") """
-    #plt.show()
+        delta_mfccs = librosa.feature.delta(mfccs)
+        delta2_mfccs = librosa.feature.delta(mfccs, order=2)
 
-    delta_mfccs = librosa.feature.delta(mfccs)
-    delta2_mfccs = librosa.feature.delta(mfccs, order=2)
-    """ plt.figure(figsize=(25, 10))
-    librosa.display.specshow(delta_mfccs,
-                         x_axis="time",
-                         sr=sr)
-    plt.colorbar(format="%+2.f") """
-
-    mfccs_features = np.concatenate((mfccs, delta_mfccs, delta2_mfccs))
+        mfccs_features = np.concatenate((mfccs, delta_mfccs, delta2_mfccs))
     
-    #print(mfccs_features.shape)
-    f.writelines('%d %d\n' % mfccs_features.shape)
-    # plt.show()
-print("Finished creating MFCC results")
+        f.writelines('%s\t%d\t%d\n' % (
+            command[2], mfccs_features.shape[0], mfccs_features.shape[1]))
+print('Finished creating MFCC results')
 f.close()
